616ccdca245fde48bc977a60a1408e487a37740c
Harshit Kedia <harshitkedia32@gmail.com>
2021-05-19 15:41:55 +0530

    remove logo

--- src/components/navHeader/NavHeader.js
+++ src/components/navHeader/NavHeader.js
@@ -2,17 +2,18 @@ import React from "react"
 import styled from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
-import ClarisightsLogo from "../../images/clarisightsLogo.svg"
+// import ClarisightsLogo from "../../images/clarisightsLogo.svg"
 
 const HeaderDiv = styled.div`
   background-color: #7986cb;
   padding: 15px 25px;
   border-bottom: 1px solid #d3d3d3;
   box-shadow: #d9d9d9 2px 2px 4px 0px;
+  min-height: 25px;
 `
-const ImgLogo = styled.img`
-  vertical-align: middle;
-`
+// const ImgLogo = styled.img`
+//   vertical-align: middle;
+// `
 const FontAwesomeIconStyled = styled(FontAwesomeIcon)`
   float: right;
   margin-top: 4px;
@@ -22,7 +23,7 @@ const FontAwesomeIconStyled = styled(FontAwesomeIcon)`
 function NavHeader() {
   return (
     <HeaderDiv data-testid="navHeader">
-      <ImgLogo src={ClarisightsLogo} />
+      {/* <ImgLogo src={ClarisightsLogo} /> */}
       <FontAwesomeIconStyled icon={faBars} />
     </HeaderDiv>
   )
